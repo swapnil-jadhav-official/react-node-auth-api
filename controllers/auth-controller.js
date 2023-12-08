1e2c5d79ad66de2cf3c95411fac1038f584b72c0
Swapnil Jadhav <109851868+swapnil-jadhav-official@users.noreply.github.com>
2023-12-08 15:34:43 +0530

    Add files via upload

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -8,13 +8,15 @@ const auth = async (req, res) => {
   let exist = await UserModel.findOne({ username });
   if(!exist) return res.status(404).send({ error : "Can't find User!"});
   console.log("User found");
-  const user = db.users.find(user => user.username === username && user.password === password);
-  if (user) {
-    const token = jwt.sign({ username }, 'secret', { expiresIn: '1h' });
-    res.render('auth', { token });
-  } else {
-    res.render('error', { error: 'Invalid username or password' });
-  }
+  //const user = db.users.find(user => user.username === username && user.password === password);
+//   if (user) {
+//     const token = jwt.sign({ username }, 'secret', { expiresIn: '1h' });
+//     res.render('auth', { token });
+//   } else {
+//     res.render('error', { error: 'Invalid username or password' });
+//   }
+
+    res.send("User exist");
 };
 
 
