eb2301817c0bed3fbb278ea2861e481d87ef8b9e
Swapnil Jadhav <109851868+swapnil-jadhav-official@users.noreply.github.com>
2023-12-08 22:18:16 +0530

    Update auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -21,13 +21,9 @@ const auth = async (req,res) =>{
                                         userId: user._id,
                                         username : user.username
                                     }, "secrete", { expiresIn : "24h"});
-
-                                   return res.render('auth', { token });
-                                                         
-
+                                   return res.render('auth', { token, username: user.username, email : user.email });                       
                     })
                     .catch(error =>{
-                        console.log(error);
                         return res.status(400).send({ error: "Password does not Match"})
                     })
             })
@@ -81,24 +77,6 @@ const register = async (req, res) => {
     return res.status(500).send(error);
   }
 };
-// const auth = async (req, res) => {
-//   req.header('Content-Type', 'application/json');
-//   const username = req.body.username;
-//   const password = req.body.password;
-//   let exist = await UserModel.findOne({ username });
-//   if(!exist) return res.status(404).send({ error : "Can't find User!"});
-//   console.log("User found");
-//   //const user = db.users.find(user => user.username === username && user.password === password);
-// //   if (user) {
-// //     const token = jwt.sign({ username }, 'secret', { expiresIn: '1h' });
-// //     res.render('auth', { token });
-// //   } else {
-// //     res.render('error', { error: 'Invalid username or password' });
-// //   }
-
-//     res.send("User exist");
-// };
-
 
 
 module.exports = { auth , register};
